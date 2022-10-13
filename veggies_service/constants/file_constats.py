8855c72f781e130d64809692584dc33490e7b098
Jatin Balani <saurav7747@gmail.com>
2022-10-13 10:43:21 +0530

    updated  code

--- veggies_service/constants/file_constats.py
+++ veggies_service/constants/file_constats.py
@@ -1,4 +1,9 @@
-#UPLOAD_PATH = '/tmp/veggies/'
 UPLOAD_PATH = '/tmp/veggies/'
 DOWNLOAD_PATH = '/data/'
 SERVER_URL = 'http://185.224.139.102'
+
+
+
+
+# UPLOAD_PATH = '/Users/jatinbalani/Downloads/document'
+
